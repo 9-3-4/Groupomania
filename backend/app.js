3e3be3435931a3eb4bf964d0d885674a088ab36f
9-3-4 <veille.dev@gmail.com>
2020-11-16 09:55:45 +0100

    mise en place ESlint/Prettier

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 //application Express
 const app = express();
 
-
-const userRoutes = require("./routes/user");
+const userRoutes = require('./routes/user');
 
 //Gestion erreur du CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');                                                                                  //tout le monde a le droit d acces a notre API
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');            // autorisation d utiliser certaines entete
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');                                            //et sur certaines methodes
+  res.setHeader('Access-Control-Allow-Origin', '*'); //tout le monde a le droit d acces a notre API
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  ); // autorisation d utiliser certaines entete
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //et sur certaines methodes
   next();
 });
 
@@ -22,18 +23,5 @@ app.use(bodyParser.json());
 
 //Middlewares
 app.use('/api/auth', userRoutes);
-
-
-
-
-/*con.connect(function(err) {
-  if (err) throw err;
-  console.log("connecté à la base de donnée ");
-
- /* con.query("SELECT t_user.id as 'ID', t_user.nom as 'NOM', FROM t_user ", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
-});*/
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,78 +1,73 @@
-
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require("./../models/user");
+const User = require('./../models/user');
 
 //Création d'un user
 exports.signup = (req, res, next) => {
-    // Valider la requète
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    };
-    bcrypt.hash(req.body.password, 10)                                                              //hashage du mot de passe
-        .then(hash => {
-            const user = new User({                                                                 // Création de l'user
-                ...req.body,
-                password: hash
-            });
-            User.create(user, (err, data) => {                                                      // Ajout utilisateur dans la database
-                if (err)
-                    res.status(500).send({
-                        message:
-                            err.message || "Some error occurred while creating the Customer."
-                    });
-                else res.status(201).send(data);
-            });
-        })
+  // Valider la requète
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Content can not be empty!',
+    });
+  }
+  bcrypt
+    .hash(req.body.password, 10) //hashage du mot de passe
+    .then((hash) => {
+      const user = new User({
+        // Création de l'user
+        ...req.body,
+        password: hash,
+      });
+      User.create(user, (err, data) => {
+        // Ajout utilisateur dans la database
+        if (err)
+          res.status(500).send({
+            message: err.message || 'Some error occurred while creating the Customer.',
+          });
+        else res.status(201).send(data);
+      });
+    });
 };
 
 //login d'un utilisateur
 exports.login = (req, res) => {
-    //recherche de l'user
-    User.findByEmail(req.body.email, (err, data) => {
-        console.log("data",data)
-        user = data[0]
-        //Si l'utilisateur n'existe pas
-        if (user.length == 0) {
-            console.log('user non trouvé')
-            res.status(401).json({ error: 'Utilisateur non trouvé' })
-        }
-        //Si l'utilisateur existe, vérification du mot de passe
-        else {
-            bcrypt.compare(req.body.password, user.motdepasse)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    //Si valide récupération de l'id et création du TOKEN
-                    res.status(200).json({
-                        userId: user.id,
-                        token: jwt.sign(
-                            { userId: user.id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        ),
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        }
-    });
+  //recherche de l'user
+  User.findByEmail(req.body.email, (err, data) => {
+    console.log('data', data);
+    user = data[0];
+    //Si l'utilisateur n'existe pas
+    if (user.length == 0) {
+      console.log('user non trouvé');
+      res.status(401).json({ error: 'Utilisateur non trouvé' });
+    }
+    //Si l'utilisateur existe, vérification du mot de passe
+    else {
+      bcrypt
+        .compare(req.body.password, user.motdepasse)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          //Si valide récupération de l'id et création du TOKEN
+          res.status(200).json({
+            userId: user.id,
+            token: jwt.sign({ userId: user.id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    }
+  });
 };
 
 //Récupération liste des utilisateurs
 exports.findAll = (req, res, next) => {
-    User.getAll((err, data) => {
-        console.log("data",data)
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving customers."
-            });
-        else res.send(data);
-    });
+  User.getAll((err, data) => {
+    console.log('data', data);
+    if (err)
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving customers.',
+      });
+    else res.send(data);
+  });
 };
-
-
