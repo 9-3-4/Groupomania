423cea9b19f9fd301fdb6c44dc16f415063413f8
9-3-4 <veille.dev@gmail.com>
2020-12-20 16:36:12 +0100

    init post

---
+++ frontend/socialnetwork/src/views/addpost.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Addpost">
+        
+        <editpost/>
+    </div>
+</template>
+
+<script>
+
+import editpost from '@/components/editpost.vue';
+
+export default {
+    name: 'addpost',
+  components: {
+      editpost,
+  },
+}
+</script>
